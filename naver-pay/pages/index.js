ab2348ba638203704d4accbd2b608821889a6a11
devsoopark <sparkm0128@gmail.com>
2019-05-16 22:16:49 +0900

    feature(pages/index): index 페이지 생성

---
+++ naver-pay/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from "next/link";
+import Layout from '../components/Layout/Layout';
+
+export default class Counter extends React.Component {
+  render() {
+    return (
+      <Layout>
+        
+      </Layout>
+    )
+  }
+}
