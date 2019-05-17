1452dce9386a220c9376773979d6ce6d5fde54f0
devsoopark <sparkm0128@gmail.com>
2019-05-17 11:00:24 +0900

    feature(pages/order): order 페이지 생성

---
+++ naver-pay/pages/order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+import Layout from '../components/Layout';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    )
+  }
+}
