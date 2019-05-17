92ba76df7fab5f08ff766ccf48c0200646c894ae
devsoopark <sparkm0128@gmail.com>
2019-05-17 10:49:17 +0900

    doc(constants): 상수 추가

---
+++ naver-pay/constants/apiUrls.js
@@ -0,0 +1,9 @@
+const PAY_GET_ACCOUNT_URL = 'http://localhost:3000/users';
+const PAY_GET_ORDERS_URL = 'http://localhost:3000/orders';
+const PAY_GET_ORDER_DETAIL_URL = 'http://localhost:3000/order';
+
+export {
+  PAY_GET_ACCOUNT_URL,
+  PAY_GET_ORDERS_URL,
+  PAY_GET_ORDER_DETAIL_URL,
+}
---
+++ naver-pay/constants/stores.js
@@ -0,0 +1,7 @@
+const CLICK_HANDLE_STORE = 'clickHandleStore';
+const ORDER_STORE = 'orderStore';
+
+export {
+  CLICK_HANDLE_STORE,
+  ORDER_STORE,
+}
