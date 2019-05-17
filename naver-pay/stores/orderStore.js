dc6f8d03f53e0ea0d77d692c51905e6d6066de32
devsoopark <sparkm0128@gmail.com>
2019-05-17 10:53:06 +0900

    feature(orderStore): 사용자 주문 내역, 상품 내역 받아오는 action 생성

---
+++ naver-pay/stores/orderStore.js
@@ -0,0 +1,55 @@
+import { observable, action, toJS } from 'mobx';
+import { getPayOrderList } from '../apis/nPayApi';
+
+export class OrderStore {
+    @observable orderListArray = [];
+    @observable itemListArray = [];
+    @observable orderDate = [];
+
+    @action
+    compStringReverse(a, b) {
+      if (a > b) return -1;
+      if (b > a) return 1;
+      return 0;
+    }
+
+    @action
+    setOrderDate = (orders) => {
+      for(let i = 0; i < orders.length; i++) {
+        let date;
+        date = orders[i].date.substr(0,10) + ' ' + orders[i].date.substr(11, 8);
+        this.orderDate.push(date)
+      }
+      this.orderDate = this.orderDate.sort(this.compStringReverse);
+    }
+
+    @action
+    getOrderList = async (userId) => {
+      await getPayOrderList(userId)
+      .then(orders => {
+        this.setOrderDate(orders.body[0].orders);
+        this.setOrderList(orders.body[0].orders);
+        this.setItemList(orders.body[0].orders);
+      });
+    };
+
+    @action
+    setOrderList = (orders) => {
+      for(let i = 0; i < 10; i++) {
+        this.orderListArray = {
+          ...this.orderListArray,
+          [this.orderDate[i]]: orders[i]
+        }
+      }
+    }
+
+    @action
+    setItemList = (orders) => {
+      for(let i = 0; i<10; i++) {
+        this.orderItemArray = {
+          ...this.orderItemArray,
+          [this.orderDate[i]]: orders[i].items
+        }
+      }
+    }
+}
